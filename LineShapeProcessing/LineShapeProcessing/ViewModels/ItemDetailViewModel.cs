e28e02c95ad2e1e182cc04e44c48cefff09e15f0
迪南 林 <eamdfan@hotmail.com>
2020-08-09 18:25:55 +0800

    添加项目文件。

---
+++ LineShapeProcessing/LineShapeProcessing/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using LineShapeProcessing.Models;
+
+namespace LineShapeProcessing.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
